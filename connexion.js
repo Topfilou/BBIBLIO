0676ff4079f322502a56a8d5ae17feb7851bb81a
Topfilou <73021536+Topfilou@users.noreply.github.com>
2021-01-08 11:53:59 +0100

    .

---
+++ connexion.js
@@ -0,0 +1,13 @@
+
+
+function nomdu(){
+    x = document.getElementById("nomdutil").innerHTML
+}
+
+
+function regexMDP(){
+var formatReg = new formatReg('^.+\.(([pP][dD][fF])|([jJ][pP][gG]))$');
+}
+
+
+document.getElementById("nomdutil").addEventListener("submit")
